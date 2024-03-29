27d1296e94fb125f0cf5a804fa27b5932c652c11
Dmitry Kukarin <dmitrykukarin@macbook-air-dmitry.home>
2024-03-29 21:19:01 +0100

    update-commit

---
+++ first_app.py
@@ -0,0 +1,20 @@
+# comment
+
+print("Test_Testing!") #comment
+print(5)
+print("Result: ", 5)
+print("Result: ", 7, 15, ".")
+print("Result: ", 7, 15, ".", sep="") # no spaces
+print("Result: ", 7, 15, ".", sep="|") # symbols or spaces
+print("Result: ", 7, 15, ".", sep="$") # symbols or spaces
+print("Result: ", 7, 15, ".", end="") # will be united with the next line
+print("Result: ", 5, 17, ".")
+print("Result: ", 5, 17, ".", end="\n") # the next line will be wrapped
+print("Result: ", 5, 17, ".")
+
+print("test \" testing") # " between words or
+print('test " testing') # " between words
+print('test \t testing') # \t - tabulation
+
+# test comment 
+print("test \n testing")
