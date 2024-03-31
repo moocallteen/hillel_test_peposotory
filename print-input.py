4e4e415591d153833b87ba8412b85d4ff592299e
Dmitry Kukarin <dmitrykukarin@macbook-air-dmitry.home>
2024-04-01 00:39:59 +0200

    print-input-first-exercises

---
+++ print-input.py
@@ -0,0 +1,40 @@
+# comment
+
+print("Test_Testing!") #comment
+print(5)
+print("Result: ", 5)
+print("Result: ", 7, 15, ".")
+print("Result: ", 7, 15, ".", sep="") # no spaces
+print("Result: ", 7, 15, ".", sep="|") # symbols or spaces
+print("Result: ", 7, 15, ".", sep="$") # symbols or spaces
+print("Result: ", 7, 15, ".", end="") # will be united with the next line
+print("Result: ", 5, 17, ".")
+print("Result: ", 5, 17, ".", end="\n") # the next line will be wrapped
+print("Result: ", 5, 17, ".")
+
+print("test \" testing") # " between words or
+print('test " testing') # " between words
+print('test \t testing') # \t - tabulation
+
+# test comment 
+print("test \n testing") # \n - to the new line
+print("test \\ slash") # slash shows special symbols, here we'll have \
+
+print(5 + 5) # summ
+print(5 - 5) 
+print(5 * 5) 
+print(5 % 10) 
+print("Result_27:", 5 + 5)
+print("Result_28:", 5 ** 5) # exponentiation (3125)
+print("Result_29:", pow(5, 6)) # exponentiation (15625)
+
+print("Result_31:", 5 // 3) # rounding
+print("Result_32:", round(5 / 3)) # rounding to closest
+
+print("Result:", min(2, 1, 0, 4, -4, -1, 3)) # finds minimal element
+print("Result:", max(2, 1, 0, 4, -4, -1, 3)) # finds maximal element
+print("Result:", abs(-6)) # number by module value
+
+# receiving data
+input()
+input("Enter your text:")
