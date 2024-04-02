a7656e61ec207323d2f0d954cad9a30d4d0061b8
Dmitry Kukarin <dmitrykukarin@macbook-air-dmitry.home>
2024-04-03 00:39:51 +0200

    if-else-operators-exercises

---
+++ python-training/if-else-operators.py
@@ -0,0 +1,89 @@
+# number_lessEqual = int(input("Your number:"))
+# number_equal = int(input("Your number:"))
+# number_more = int(input("Your number:"))
+# number_less = int(input("Your number:"))
+# number_notEqual = int(input("Your number:"))
+
+# 12
+num1 = int(input("Your 1st number:"))
+num2 = int(input("Your 2nd number:"))
+
+if num1 == 5 and num2 != 5:
+    print("Success!")
+elif num1 == num2:
+    print("What a twist!")
+    if num1 and num2 < 5:
+        print("Try bigger numbers!")
+
+
+# 11
+# number_notEqual = int(input("Your number:"))
+# if number_notEqual == 5:
+#     print("Success!")
+# elif number_notEqual <= 5:
+#     print("Nice try")
+# else:
+#     print("Use another try :(")
+
+
+# 10
+# isHappy = False
+# if not isHappy: # this form works only for boolean
+#     print("Good")
+
+# isHappy = True
+# if isHappy: 
+#     print("Good")
+
+# 9
+# user_data2 = int(input("Your user data number:"))
+# if user_data2 >= 5:
+#     print("Nice")
+# else:
+#     user_data2 < 5
+#     print("Gatcha!")
+
+# 1
+# number_moreEqual = int(input("Your number:"))
+# if number_moreEqual >= 5: 
+#     print("Accepted")
+#     if number_moreEqual == 7: 
+#         print("Accepted")
+#     else:
+#         print("Error")
+# else:
+#     print("O4ko")
+
+# 2
+# number_equal = int(input("Your number:"))
+# if number_equal == 5: 
+#     print("Accepted")
+# else:
+#     print("Epic Fail")
+
+# 3
+# if number_more > 5: 
+#     print("Accepted")
+
+# 4
+# if number_less < 5: 
+#     print("Accepted")
+
+# 5
+# if number_notEqual != 5: 
+#     print("Accepted")
+    # else number < 5:
+    # print("Error")
+
+# 7
+# if 5 == 5:
+#     print("Accept")
+
+# 8
+# user_data1 = int(input("Your user data number:"))
+# if user_data1 != 5: # both conditions must be satisfied
+#     print("Nice")
+#     if user_data1 > 5:
+#         print("Gatcha!")
+
+
