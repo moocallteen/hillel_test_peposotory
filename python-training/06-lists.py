191a9c7070260c92901708b02d5ff9a58b3c7a0a
Dmitry Kukarin <dmitrykukarin@macbook-air-dmitry.home>
2024-04-10 01:33:12 +0200

    lists-initial-exercises

--- python-training/06-lists.py
+++ python-training/06-lists.py
@@ -1,5 +1,24 @@
+# months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", ]
+# print(months)
+
 # it's allowed to add different types of data in the same list
+# data = [1, 2, "3", True, 6.7]
+# print(data)
 
-months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", ]
+# change a list's element
+# data = [1, 2, "3", True] 
+# data[0] = 50
+# print(data)
 
-print(months)
+# data = [1, 2, "3", True, 6.7]
+# data[3] = False
+# print(data)
+
+# output certain lement of the list
+# data = [1, 2, "3", True, 6.7]
+# print(data[4])
+
+# lists can contain lists
+data = [1, 2, "3", True, 6.7, [10, 11, 12]]
+# print(data)
+print(data[-1][0]) # output of inner list data
